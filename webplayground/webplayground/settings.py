b4053ad58a739bc723cad654688067cd05752fec
jhoysser <72633726+jhoysser@users.noreply.github.com>
2020-10-09 18:53:01 -0500

    Update settings.py

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'webplayground',
         'USER': 'postgres',
-        'PASSWORD': 'koseki',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
